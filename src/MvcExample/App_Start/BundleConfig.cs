4246e654e2e377ae96af5b067bfdd28dfe9a00e6
dgates82 <48529034+dgates82@users.noreply.github.com>
2026-07-09 11:36:10 +0100

    fix: removing minification from bootstrap bundle since its no longer compatible

--- src/MvcExample/App_Start/BundleConfig.cs
+++ src/MvcExample/App_Start/BundleConfig.cs
@@ -1,4 +1,3 @@
-using System.Web;
 using System.Web.Optimization;
 
 namespace MvcExample
@@ -14,13 +13,10 @@ namespace MvcExample
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js"));
+            var bootstrapBundle = new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js");
+            bootstrapBundle.Transforms.Clear();
+            bundles.Add(bootstrapBundle);
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
